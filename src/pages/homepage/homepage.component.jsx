fd1fba97c53b5d795cad5c3fb952ff3bb95481fd
Olabiyi Samuel <olabiyi.sam@gmail.com>
2020-10-05 21:46:09 +0100

    broken homepage into smaller component

---
+++ src/pages/homepage/homepage.component.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+import './homepage.styles.scss';
+import {Directory} from '../../components/directory/directory.component'
+
+export const HomePage = () => (
+    <div className='homepage'>
+        <Directory></Directory>
+    </div>
+);
